3cb481d1f1800260cb92b2186bf254752e8f0047
qusco <anis.aarab1998@gmail.com>
2021-04-20 14:50:34 +0200

    ff

--- Scenes/Game.py
+++ Scenes/Game.py
@@ -1,47 +1,59 @@
 import pygame
 import os
 
+pygame.init()
+
 image_path = os.path.dirname(__file__) + '/images/'
 screenWidth = 800
 screenHeight = 600
-pygame.display.set_mode((screenWidth, screenHeight))
+screen = pygame.display.set_mode((screenWidth, screenHeight))
 
-pygame.image.load(image_path + 'mainplayer.png')
+playerImage = pygame.image.load(image_path + 'mainplayer.png')
 
 
 class spriteSheet(object):
-    
+    sprite_sheet = None
+
+    def __init__(self, file_name):
+        self.sprite_sheet = pygame.image.load(file_name).convert()
+
+    def get_image(self, x, y, width, height):
+        # Creeer een lege image
+        image = pygame.Surface([width, height]).convert()
+
+        # Kopieer het de uitgeknipte sprite naar het lege image
+        image.blit(self.sprite_sheet, (0, 0), (x, y, width, height))
+
+        # Assuming black works as the transparent color
+        image.set_colorkey(constants.BLACK)
+
+        # Return the image
+        return image
 
 
 class player(pygame.sprite.Sprite):
     def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
         self.player = pygame.sprite.Group()
 
+        self.change_x = 0
+        self.change_y = 0
 
-class enemies():
-    pass
+        self.walking_frames_l = []
+        self.walking_frames_r = []
 
+        self.direction = "R"
 
-class coins():
-    pass
-
-
-class traps():
-    pass
+        image = playerImage.get_image(0, 0, 50, 50)
+        self.walking_frames_r.append(image)
 
 
 p = player()
-
-
-def main():
-    print(p.player)
-
-
 running = True
 
 while running:
+    screen.blit(p.walking_frames_r[0], (14, 44))
+    pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-
-main()
