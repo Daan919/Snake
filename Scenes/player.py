6b11f1d52e593b9051438cf68ef7224f07f3d2b5
Thij Hakvoort <TBW.Hakvoort@student.han.nl>
2021-04-21 10:04:06 +0200

    ok

--- Scenes/player.py
+++ Scenes/player.py
@@ -6,23 +6,93 @@ from spritesheet_function import SpriteSheet
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
-        pass
+        super().__init__()
+
+        self.change_x = 0
+        self.change_y = 0
+
+        self.walking_frames_l = []
+        self.walking_frames_r = []
+
+        self.direction = "R"
+
+        self.level = None
+
+        sprite_sheet = SpriteSheet("MoetNogGemaaktWorden.png")
+
+        #Rechts
+
+        image = sprite_sheet.get_image(0, 0, 66, 90)
+        self.walking_frames_r.append(image)
+        image = sprite_sheet.get_image(66, 0, 66, 90)
+        self.walking_frames_r.append(image)
+        image = sprite_sheet.get_image(132, 0, 67, 90)
+        self.walking_frames_r.append(image)
+        image = sprite_sheet.get_image(0, 93, 66, 90)
+        self.walking_frames_r.append(image)
+        image = sprite_sheet.get_image(66, 93, 66, 90)
+        self.walking_frames_r.append(image)
+        image = sprite_sheet.get_image(132, 93, 72, 90)
+        self.walking_frames_r.append(image)
+        image = sprite_sheet.get_image(0, 186, 70, 90)
+        self.walking_frames_r.append(image)
+
+        #Links
+
+        image = sprite_sheet.get_image(0, 0, 66, 90)
+        image = pygame.transform.flip(image, True, False)
+        self.walking_frames_l.append(image)
+        image = sprite_sheet.get_image(66, 0, 66, 90)
+        image = pygame.transform.flip(image, True, False)
+        self.walking_frames_l.append(image)
+        image = sprite_sheet.get_image(132, 0, 67, 90)
+        image = pygame.transform.flip(image, True, False)
+        self.walking_frames_l.append(image)
+        image = sprite_sheet.get_image(0, 93, 66, 90)
+        image = pygame.transform.flip(image, True, False)
+        self.walking_frames_l.append(image)
+        image = sprite_sheet.get_image(66, 93, 66, 90)
+        image = pygame.transform.flip(image, True, False)
+        self.walking_frames_l.append(image)
+        image = sprite_sheet.get_image(132, 93, 72, 90)
+        image = pygame.transform.flip(image, True, False)
+        self.walking_frames_l.append(image)
+        image = sprite_sheet.get_image(0, 186, 70, 90)
+        image = pygame.transform.flip(image, True, False)
+        self.walking_frames_l.append(image)
+        
+        self.image = self.walking_frames_r[0]
+
+        self.rect = self.image.get_rect()
+
+ 
     
     def update(self):
         pass
 
     def calc_grav(self):
-        pass
+        if self.change_y == 0:
+            self.change_y = 1
+        else:
+            self.change_y += .35
+
+        if self.rect.y >= constants.SCREEN_HEIGHT - self.rect.height and self.change_y >= 0:
+            self.change_y = 0
+            self.rect.y = constants.SCREEN_HEIGHT - self.rect.height
 
     def jump(self):
-        pass
+        self.rect.y += 2
+        platform_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
+        self.rect.y -= 2
 
     def go_left(self):
-        pass
+        self.change_x = -6
+        self.direction = "L"
 
     def go_right(self):
-        pass
+        self.change_x = 6
+        self.direction = "R"
     
     def stop(self):
-        pass
+        self.change_x = 0 
 
