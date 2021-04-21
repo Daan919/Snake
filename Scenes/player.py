f1e40e498a0c92692f6c238494cda9fa17ed4526
Thij Hakvoort <TBW.Hakvoort@student.han.nl>
2021-04-21 09:54:21 +0200

    ok

--- Scenes/player.py
+++ Scenes/player.py
@@ -5,3 +5,24 @@ from platforms import MovingPlatform
 from spritesheet_function import SpriteSheet
 
 class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pass
+    
+    def update(self):
+        pass
+
+    def calc_grav(self):
+        pass
+
+    def jump(self):
+        pass
+
+    def go_left(self):
+        pass
+
+    def go_right(self):
+        pass
+    
+    def stop(self):
+        pass
+
