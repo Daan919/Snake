4041e6f6551a5e336e15dcb7b0da9bbb2ee6fb35
YoeckHeijmen <yn.heijmen@student.han.nl>
2021-04-21 10:46:09 +0200

    k

---
+++ spritesheet_functions.py
@@ -0,0 +1,15 @@
+import pygame
+import constants
+
+class SpriteSheet(object):
+    def __init__(self, file_name):
+        self.sprite_sheet = pygame.image.load(file_name).convert()
+ 
+ 
+    def getImage(self, x, y, width, height):
+        image = pygame.Surface([width, height]).convert()
+        image.blit(self.sprite_sheet, (0, 0), (x, y, width, height))
+        image.set_colorkey(constants.black)
+
+        return image
+        
