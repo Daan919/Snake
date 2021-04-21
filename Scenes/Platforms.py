495bb8e3fef39f3ff6c844ad5091273c35b1d879
YoeckHeijmen <yn.heijmen@student.han.nl>
2021-04-21 10:25:12 +0200

    k

--- Scenes/Platforms.py
+++ Scenes/Platforms.py
@@ -2,9 +2,10 @@ import pygame
 
 from spritesheet_functions import SpriteSheet
 
-GRASS_LEFT            = (576, 720, 70, 70)
-GRASS_RIGHT           = (576, 576, 70, 70)
-GRASS_MIDDLE          = (504, 576, 70, 70)
+
+WATER_LEFT            = (500, 720, 70, 70)
+WATER_RIGHT           = (500, 720, 70, 70)
+WATER_MIDDLE          = (425, 640, 70, 70)
 STONE_PLATFORM_LEFT   = (432, 720, 70, 40)
 STONE_PLATFORM_MIDDLE = (648, 648, 70, 40)
 STONE_PLATFORM_RIGHT  = (792, 648, 70, 40)
