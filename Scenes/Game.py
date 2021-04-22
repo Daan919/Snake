fbf924be3eaa86ded3fad9cb4c340312cfc1ce43
daan <daan9191@gmail.com>
2021-04-22 11:15:16 +0200

    draw world

--- Scenes/Game.py
+++ Scenes/Game.py
@@ -5,12 +5,36 @@ pygame.init()
 pygame.display.init()
 clock = pygame.time.Clock()
 
-screenWidth = 800
-screenHeight = 600
+screenWidth = 1000
+screenHeight = 1000
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 image_path = os.path.dirname(__file__) + '/images/'
 
 BLUE = (0,   0, 255)
+tile_size = 50
+
+world_data = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+]
 
 
 class background():
@@ -22,60 +46,63 @@ class background():
             self.background, (screenWidth, screenHeight))
 
     def draw(self, screen):
-        screen.fill(BLUE)
         screen.blit(self.background, (0, 0))
 
 
 class level(pygame.sprite.Sprite):
 
-    def __init__(self):
+    def __init__(self, data):
+        self.tile_list = []
         self.coin_list = pygame.sprite.Group()
         self.spike_list = pygame.sprite.Group()
         self.platform_list = pygame.sprite.Group()
 
-        platform_smal = pygame.image.load(image_path + "")
+        self.platform_img = pygame.image.load(image_path + "dirt.png")
+        self.dirt = self.platform_img
+        self.dirt = pygame.transform.scale(
+            self.dirt, (tile_size, tile_size))
+        self.platform_img = pygame.image.load(image_path + "grass.png")
+        self.grass = self.platform_img
+        self.grass = pygame.transform.scale(
+            self.grass, (tile_size, tile_size))
 
-    def platform(self):
+        row_count = 0
+        for row in data:
+            colum_count = 0
+            for tile in row:
+                if tile == 1:
+                    img_rect = self.dirt.get_rect()
+                    img_rect.x = colum_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (self.dirt, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 2:
+                    img_rect = self.grass.get_rect()
+                    img_rect.x = colum_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (self.grass, img_rect)
+                    self.tile_list.append(tile)
+                colum_count += 1
+            row_count += 1
 
-        pygame.sprite.Sprite.__init__(self)
-
-        sprite_sheet = spriteSheet(
-            image_path + 'platforms.png')
-
-        image = sprite_sheet.get_image(0, 0, 132, 978)
-        self.platforms.append(iamge)
-        self.rect = self.platforms.get_rect()
-
-    def draw(self):
-        self.platform_list.draw(screen)
-
-    def level1(self):
-
-        level = [grootPlatform, 50, 400]
-
-        for platform in level:
-            block = platform[0]
-            blockX = platform[1]
-            blockY = platform[2]
-            self.platform_list.add(block)
+    def draw(self,):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
 
 
 def main():
 
     bg = background()
-    lv = level()
-    lv.platform()
-
+    lv = level(world_data)
     running = True
 
     while running:
         clock.tick(60)
 
         bg.draw(screen)
-
-        lv.level1()
         lv.draw()
         pygame.display.flip()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
