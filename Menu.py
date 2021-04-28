3e4c459531a6ee0d26910d9830b5ca9857f7aca9
Thij Hakvoort <TBW.Hakvoort@student.han.nl>
2021-04-28 16:11:05 +0200

    ok

--- Menu.py
+++ Menu.py
@@ -1,5 +1,6 @@
 import pygame, sys
 import NewGame
+import os
 from pygame.locals import *
 
 
@@ -8,8 +9,21 @@ pygame.init()
 pygame.display.set_caption('Menu')
 size = [500, 500]
 screen = pygame.display.set_mode(size)
+image_path = os.path.dirname(__file__) + '/Images/'
+
 
 font = pygame.font.SysFont(None, 20)
+
+class background():
+    def __init__(self):
+        self.background = None
+        self.background = pygame.image.load(
+            image_path + 'Menu.png').convert()
+        self.background = pygame.transform.scale(
+            self.background, (500, 500))
+
+    def draw(self, screen):
+        screen.blit(self.background, (0, 0))
 
 def draw_text(text, font, color, surface, x , y):
     textobj = font.render(text, 1, color)
@@ -18,18 +32,20 @@ def draw_text(text, font, color, surface, x , y):
     surface.blit(textobj, textrect)
 
 click = False
+bg = background()
 
 def main_menu():
-    
     while True:
 
         screen.fill((0,0,0))
+        bg.draw(screen)
+        
         
         mx, my = pygame.mouse.get_pos()
 
-        button_1 = pygame.Rect(50, 100, 200, 50)
-        button_2 = pygame.Rect(50, 200, 200, 50)
-        button_3 = pygame.Rect(50, 300, 200, 50)
+        button_1 = pygame.Rect(140, 156, 220, 50)
+        button_2 = pygame.Rect(140, 256, 220, 50)
+        button_3 = pygame.Rect(140, 356, 220, 50)
 
         if button_1.collidepoint((mx, my)):
             if click:
@@ -42,14 +58,13 @@ def main_menu():
                 pygame.quit()
                 sys.exit()
 
-        pygame.draw.rect(screen, (255, 0, 0), button_1)
-        pygame.draw.rect(screen, (255, 0, 0), button_2)
-        pygame.draw.rect(screen, (255, 0, 0), button_3)
+        pygame.draw.rect(screen, (255, 0, 0), button_1, 1)
+        pygame.draw.rect(screen, (255, 0, 0), button_2, 1)
+        pygame.draw.rect(screen, (255, 0, 0), button_3, 1)
 
-        draw_text('Menu', font, (255, 255, 255), screen, 20, 20)
-        draw_text('Game', font, (255, 255, 255), screen, 50, 100)
-        draw_text('Options', font, (255, 255, 255), screen, 50, 200)
-        draw_text('Quit', font, (255, 255, 255), screen, 50, 300)
+        draw_text('Game', font, (255, 255, 255), screen, 230, 165)
+        draw_text('Options', font, (255, 255, 255), screen, 230, 265)
+        draw_text('Quit', font, (255, 255, 255), screen, 230, 365)
 
         click = False
         for event in pygame.event.get():
